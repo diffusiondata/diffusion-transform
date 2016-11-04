efd0e7f5b9e89206725f686ce5e02fd3e5d91ee1
Matt Champion <mattunderscorechampion@gmail.com>
2016-11-04 21:09:42 +0000

    Add an AbstractTransformer to simplify the handling of exceptions.

---
+++ transformers/src/main/java/com/pushtechnology/diffusion/transform/transformer/AbstractTransformer.java
@@ -0,0 +1,50 @@
+/*******************************************************************************
+ * Copyright (C) 2016 Push Technology Ltd.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *******************************************************************************/
+
+package com.pushtechnology.diffusion.transform.transformer;
+
+/**
+ * Abstract {@link Transformer} that catches and converts any exception to a
+ * {@link TransformationException}.
+ *
+ * @param <S> the type of the source values
+ * @param <T> the type of the transformed values
+ * @author Push Technology Limited
+ */
+public abstract class AbstractTransformer<S, T> implements Transformer<S, T> {
+    @Override
+    public final T transform(S value) throws TransformationException {
+        try {
+            return transformUnsafely(value);
+        }
+        catch (TransformationException e) {
+            throw e;
+        }
+        // CHECKSTYLE.OFF: IllegalCatch // Bulkhead
+        catch (Exception e) {
+            throw new TransformationException(e);
+        }
+        // CHECKSTYLE.ON: IllegalCatch // Bulkhead
+    }
+
+    /**
+     * Transform the value.
+     *
+     * @param value the value to transform
+     * @return the transformed value
+     * @throws Exception if the transformation cannot be applied
+     */
+    protected abstract T transformUnsafely(S value) throws Exception;
+}
---
+++ transformers/src/test/java/com/pushtechnology/diffusion/transform/transformer/AbstractTransformerTest.java
@@ -0,0 +1,68 @@
+/*******************************************************************************
+ * Copyright (C) 2016 Push Technology Ltd.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *******************************************************************************/
+
+package com.pushtechnology.diffusion.transform.transformer;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+/**
+ * Unit tests for {@link AbstractTransformer}.
+ *
+ * @author Push Technology Limited
+ */
+public final class AbstractTransformerTest {
+
+    @Test
+    public void transform() throws TransformationException {
+        final IdentityTransformer transformer = new IdentityTransformer();
+        final String transformedValue = transformer.transform("value");
+        assertEquals("value", transformedValue);
+    }
+
+    @Test(expected = TransformationException.class)
+    public void failure() throws TransformationException {
+        final ThrowingTransformer transformer = new ThrowingTransformer();
+        transformer.transform("value");
+    }
+
+    @Test(expected = TransformationException.class)
+    public void transformationException() throws TransformationException {
+        final TransformationExceptionThrowingTransformer transformer = new TransformationExceptionThrowingTransformer();
+        transformer.transform("value");
+    }
+
+    private static final class IdentityTransformer extends AbstractTransformer<String, String> {
+        @Override
+        protected String transformUnsafely(String value) throws Exception {
+            return value;
+        }
+    }
+
+    private static final class ThrowingTransformer extends AbstractTransformer<String, String> {
+        @Override
+        protected String transformUnsafely(String value) throws Exception {
+            throw new Exception("Intentionally thrown by test");
+        }
+    }
+
+    private static final class TransformationExceptionThrowingTransformer extends AbstractTransformer<String, String> {
+        @Override
+        protected String transformUnsafely(String value) throws Exception {
+            throw new Exception("Intentionally thrown by test");
+        }
+    }
+}
