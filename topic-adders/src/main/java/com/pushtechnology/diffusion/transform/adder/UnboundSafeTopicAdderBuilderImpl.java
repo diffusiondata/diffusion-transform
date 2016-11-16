03b18c4b9ccc6e557f93618835f08287a2fac306
Matt Champion <mattunderscorechampion@gmail.com>
2016-11-16 21:50:37 +0000

    Add unbound safe and unbound transformed topic adder builders.

---
+++ topic-adders/src/main/java/com/pushtechnology/diffusion/transform/adder/UnboundSafeTopicAdderBuilderImpl.java
@@ -0,0 +1,92 @@
+/*******************************************************************************
+ * Copyright (C) 2016 Push Technology Ltd.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *******************************************************************************/
+
+package com.pushtechnology.diffusion.transform.adder;
+
+import static com.pushtechnology.diffusion.transform.transformer.Transformers.chain;
+import static com.pushtechnology.diffusion.transform.transformer.Transformers.toTransformer;
+
+import com.pushtechnology.diffusion.client.features.control.topics.TopicControl;
+import com.pushtechnology.diffusion.client.session.Session;
+import com.pushtechnology.diffusion.client.topics.details.TopicType;
+import com.pushtechnology.diffusion.datatype.Bytes;
+import com.pushtechnology.diffusion.transform.transformer.SafeTransformer;
+import com.pushtechnology.diffusion.transform.transformer.Transformer;
+import com.pushtechnology.diffusion.transform.transformer.UnsafeTransformer;
+
+/**
+ * Implementation of {@link UnboundSafeTopicAdderBuilder}.
+ *
+ * @param <S> The type of value understood by the topic
+ * @param <T> The type of value updates are provided as
+ * @author Push Technology Limited
+ */
+/*package*/ final class UnboundSafeTopicAdderBuilderImpl<S extends Bytes, T>
+    implements UnboundSafeTopicAdderBuilder<S, T> {
+
+    private final TopicType topicType;
+    private final SafeTransformer<T, S> transformer;
+
+    UnboundSafeTopicAdderBuilderImpl(TopicType topicType, SafeTransformer<T, S> transformer) {
+        this.topicType = topicType;
+        this.transformer = transformer;
+    }
+
+    @Override
+    public <R> UnboundSafeTopicAdderBuilder<S, R> transform(SafeTransformer<R, T> newTransformer) {
+        return new UnboundSafeTopicAdderBuilderImpl<>(topicType, chain(newTransformer, transformer));
+    }
+
+    @Override
+    public <R> UnboundSafeTopicAdderBuilder<S, R> transform(SafeTransformer<R, T> newTransformer, Class<R> type) {
+        return new UnboundSafeTopicAdderBuilderImpl<>(topicType, chain(newTransformer, transformer));
+    }
+
+    @Override
+    public <R> UnboundTransformedTopicAdderBuilder<S, R> transform(Transformer<R, T> newTransformer) {
+        return new UnboundTransformedTopicAdderBuilderImpl<>(topicType, chain(newTransformer, transformer));
+    }
+
+    @Override
+    public <R> UnboundTransformedTopicAdderBuilder<S, R> transform(Transformer<R, T> newTransformer, Class<R> type) {
+        return new UnboundTransformedTopicAdderBuilderImpl<>(topicType, chain(newTransformer, transformer));
+    }
+
+    @Override
+    public <R> UnboundTransformedTopicAdderBuilder<S, R> transformWith(UnsafeTransformer<R, T> newTransformer) {
+        return new UnboundTransformedTopicAdderBuilderImpl<>(
+            topicType,
+            chain(toTransformer(newTransformer), transformer));
+    }
+
+    @Override
+    public <R> UnboundTransformedTopicAdderBuilder<S, R> transformWith(
+            UnsafeTransformer<R, T> newTransformer,
+            Class<R> type) {
+        return new UnboundTransformedTopicAdderBuilderImpl<>(
+            topicType,
+            chain(toTransformer(newTransformer), transformer));
+    }
+
+    @Override
+    public BoundTopicAdderBuilder<S, T, SafeTopicAdder<T>> bind(Session session) {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public SafeTopicAdder<T> create(Session session) {
+        return new SafeTopicAdderImpl<>(session.feature(TopicControl.class), topicType, transformer);
+    }
+}
---
+++ topic-adders/src/test/java/com/pushtechnology/diffusion/transform/adder/UnboundTransformedTopicAdderBuilderImplTest.java
@@ -0,0 +1,119 @@
+/*******************************************************************************
+ * Copyright (C) 2016 Push Technology Ltd.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *******************************************************************************/
+
+package com.pushtechnology.diffusion.transform.adder;
+
+import static com.pushtechnology.diffusion.client.topics.details.TopicType.BINARY;
+import static com.pushtechnology.diffusion.transform.transformer.Transformers.identity;
+import static com.pushtechnology.diffusion.transform.transformer.Transformers.stringToBinary;
+import static java.nio.charset.Charset.forName;
+import static org.junit.Assert.assertNotNull;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.verifyNoMoreInteractions;
+import static org.mockito.Mockito.when;
+import static org.mockito.MockitoAnnotations.initMocks;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.Mock;
+
+import com.pushtechnology.diffusion.client.features.control.topics.TopicControl;
+import com.pushtechnology.diffusion.client.session.Session;
+import com.pushtechnology.diffusion.datatype.binary.Binary;
+import com.pushtechnology.diffusion.transform.transformer.UnsafeTransformer;
+
+/**
+ * Unit tests for {@link UnboundSafeTopicAdderBuilderImpl}.
+ *
+ * @author Push Technology Limited
+ */
+public final class UnboundTransformedTopicAdderBuilderImplTest {
+
+    @Mock
+    private Session session;
+    @Mock
+    private TopicControl control;
+
+    @Before
+    public void setUp() {
+        initMocks(this);
+
+        when(session.feature(TopicControl.class)).thenReturn(control);
+    }
+
+    @After
+    public void postConditions() {
+        verifyNoMoreInteractions(session, control);
+    }
+
+    @Test
+    public void transform() {
+        final UnboundTransformedTopicAdderBuilder<Binary, String> builder =
+            new UnboundTransformedTopicAdderBuilderImpl<>(BINARY, identity(Binary.class))
+                .transform(stringToBinary(forName("UTF-8")));
+
+        assertNotNull(builder);
+    }
+
+    @Test
+    public void transformToClass() {
+        final UnboundTransformedTopicAdderBuilder<Binary, String> builder =
+            new UnboundTransformedTopicAdderBuilderImpl<>(BINARY, identity(Binary.class))
+                .transform(stringToBinary(forName("UTF-8")), String.class);
+
+        assertNotNull(builder);
+    }
+
+    @Test
+    public void transformWith() {
+        final UnboundTransformedTopicAdderBuilder<Binary, String> builder =
+            new UnboundTransformedTopicAdderBuilderImpl<>(BINARY, identity(Binary.class))
+                .transformWith(new UnsafeTransformer<String, Binary>() {
+                    @Override
+                    public Binary transform(String value) throws Exception {
+                        return null;
+                    }
+                });
+
+        assertNotNull(builder);
+    }
+
+    @Test
+    public void transformWithToClass() {
+        final UnboundTransformedTopicAdderBuilder<Binary, String> builder =
+            new UnboundTransformedTopicAdderBuilderImpl<>(BINARY, identity(Binary.class))
+                .transformWith(new UnsafeTransformer<String, Binary>() {
+                    @Override
+                    public Binary transform(String value) throws Exception {
+                        return null;
+                    }
+                }, String.class);
+
+        assertNotNull(builder);
+    }
+
+    @Test
+    public void create() {
+        final UnboundTransformedTopicAdderBuilderImpl<Binary, String> builder =
+            new UnboundTransformedTopicAdderBuilderImpl<>(BINARY, stringToBinary(forName("UTF-8")));
+
+        final TopicAdder<String> adder = builder.create(session);
+
+        assertNotNull(adder);
+
+        verify(session).feature(TopicControl.class);
+    }
+}
