5d9df16d1df52e77b255e13174568d0bbbab53aa
Matt Champion <mattunderscorechampion@gmail.com>
2016-12-06 22:01:26 +0000

    Implement builders for message streams.

---
+++ messaging/src/main/java/com/pushtechnology/diffusion/transform/messaging/UnboundSafeMessageStreamBuilderImpl.java
@@ -0,0 +1,64 @@
+/*******************************************************************************
+ * Copyright (C) 2016 Push Technology Ltd.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *******************************************************************************/
+
+package com.pushtechnology.diffusion.transform.messaging;
+
+import static com.pushtechnology.diffusion.transform.transformer.Transformers.chain;
+import static com.pushtechnology.diffusion.transform.transformer.Transformers.toTransformer;
+
+import com.pushtechnology.diffusion.client.content.Content;
+import com.pushtechnology.diffusion.client.features.Messaging;
+import com.pushtechnology.diffusion.client.session.Session;
+import com.pushtechnology.diffusion.transform.transformer.SafeTransformer;
+import com.pushtechnology.diffusion.transform.transformer.Transformer;
+import com.pushtechnology.diffusion.transform.transformer.UnsafeTransformer;
+
+/**
+ * Implementation of {@link UnboundSafeMessageStreamBuilder}.
+ *
+ * @param <V> the type of values
+ * @author Push Technology Limited
+ */
+/*package*/ final class UnboundSafeMessageStreamBuilderImpl<V> implements UnboundSafeMessageStreamBuilder<V> {
+    private final SafeTransformer<Content, V> transformer;
+
+    UnboundSafeMessageStreamBuilderImpl(SafeTransformer<Content, V> transformer) {
+        this.transformer = transformer;
+    }
+
+    @Override
+    public <R> UnboundTransformedMessageStreamBuilder<R> transform(Transformer<V, R> newTransformer) {
+        return new UnboundTransformedMessageStreamBuilderImpl<>(chain(transformer, newTransformer));
+    }
+
+    @Override
+    public <R> UnboundTransformedMessageStreamBuilder<R> transformWith(UnsafeTransformer<V, R> newTransformer) {
+        return new UnboundTransformedMessageStreamBuilderImpl<>(chain(transformer, toTransformer(newTransformer)));
+    }
+
+    @Override
+    public <R> UnboundSafeMessageStreamBuilder<R> transform(SafeTransformer<V, R> newTransformer) {
+        return new UnboundSafeMessageStreamBuilderImpl<>(chain(transformer, newTransformer));
+    }
+
+    @Override
+    public MessageStreamHandle register(Session session, SafeMessageStream<V> stream) {
+        final Messaging.MessageStream adapter = new SafeMessageStreamAdapter<>(transformer, stream);
+        final Messaging messaging = session.feature(Messaging.class);
+        final MessageStreamHandleImpl handle = new MessageStreamHandleImpl(messaging, adapter);
+        messaging.addFallbackMessageStream(adapter);
+        return handle;
+    }
+}
---
+++ messaging/src/test/java/com/pushtechnology/diffusion/transform/messaging/BoundSafeMessageStreamBuilderImplTest.java
@@ -0,0 +1,99 @@
+/*******************************************************************************
+ * Copyright (C) 2016 Push Technology Ltd.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *******************************************************************************/
+
+package com.pushtechnology.diffusion.transform.messaging;
+
+import static org.junit.Assert.assertNotSame;
+import static org.mockito.Matchers.isA;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+import static org.mockito.MockitoAnnotations.initMocks;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.Mock;
+
+import com.pushtechnology.diffusion.client.content.Content;
+import com.pushtechnology.diffusion.client.features.Messaging;
+import com.pushtechnology.diffusion.client.session.Session;
+import com.pushtechnology.diffusion.transform.transformer.SafeTransformer;
+import com.pushtechnology.diffusion.transform.transformer.Transformer;
+import com.pushtechnology.diffusion.transform.transformer.UnsafeTransformer;
+
+/**
+ * Unit tests for {@link BoundSafeMessageStreamBuilderImpl}.
+ *
+ * @author Push Technology Limited
+ */
+public final class BoundSafeMessageStreamBuilderImplTest {
+    @Mock
+    private Messaging messaging;
+    @Mock
+    private Session session;
+    @Mock
+    private SafeTransformer<Content, String> contentTransformer;
+    @Mock
+    private Transformer<String, String> stringTransformer;
+    @Mock
+    private UnsafeTransformer<String, String> unsafeTransformer;
+    @Mock
+    private SafeTransformer<String, String> safeStringTransformer;
+    @Mock
+    private SafeMessageStream<String> messageStream;
+
+    @Before
+    public void setUp() {
+        initMocks(this);
+
+        when(session.feature(Messaging.class)).thenReturn(messaging);
+    }
+
+    @Test
+    public void transformSafe() {
+        final BoundSafeMessageStreamBuilder<String> builder =
+            new BoundSafeMessageStreamBuilderImpl<>(contentTransformer, session);
+        final BoundSafeMessageStreamBuilder<String> newBuilder = builder.transform(safeStringTransformer);
+
+        assertNotSame(builder, newBuilder);
+    }
+
+    @Test
+    public void transform() {
+        final BoundSafeMessageStreamBuilder<String> builder =
+            new BoundSafeMessageStreamBuilderImpl<>(contentTransformer, session);
+        final BoundTransformedMessageStreamBuilder<String> newBuilder = builder.transform(stringTransformer);
+
+        assertNotSame(builder, newBuilder);
+    }
+
+    @Test
+    public void transformWith() {
+        final BoundSafeMessageStreamBuilder<String> builder =
+            new BoundSafeMessageStreamBuilderImpl<>(contentTransformer, session);
+        final BoundTransformedMessageStreamBuilder<String> newBuilder = builder.transformWith(unsafeTransformer);
+
+        assertNotSame(builder, newBuilder);
+    }
+
+    @Test
+    public void register() {
+        final BoundSafeMessageStreamBuilder<String> builder =
+            new BoundSafeMessageStreamBuilderImpl<>(contentTransformer, session);
+        builder.register(messageStream);
+
+        verify(session).feature(Messaging.class);
+        verify(messaging).addFallbackMessageStream(isA(Messaging.MessageStream.class));
+    }
+}
