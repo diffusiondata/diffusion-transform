80fe45c95a64fd5fe3606d74a0787c9aff60acc8
Matt Champion <mattunderscorechampion@gmail.com>
2017-04-12 21:55:37 +0100

    Add integration test for sending transformed messages from a session to a handler.

---
+++ messaging/src/test/java/com/pushtechnology/diffusion/transform/messaging/SessionToHandlerIT.java
@@ -0,0 +1,114 @@
+package com.pushtechnology.diffusion.transform.messaging;
+
+import static com.pushtechnology.diffusion.client.session.Session.State.CLOSED_BY_CLIENT;
+import static com.pushtechnology.diffusion.client.session.Session.State.CONNECTED_ACTIVE;
+import static com.pushtechnology.diffusion.client.session.Session.State.CONNECTING;
+import static org.mockito.Mockito.timeout;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.verifyNoMoreInteractions;
+import static org.mockito.MockitoAnnotations.initMocks;
+
+import java.util.Collections;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.Mock;
+import org.mockito.verification.VerificationWithTimeout;
+
+import com.pushtechnology.diffusion.client.Diffusion;
+import com.pushtechnology.diffusion.client.content.Content;
+import com.pushtechnology.diffusion.client.features.Messaging.SendCallback;
+import com.pushtechnology.diffusion.client.session.Session;
+import com.pushtechnology.diffusion.datatype.Bytes;
+import com.pushtechnology.diffusion.datatype.binary.Binary;
+import com.pushtechnology.diffusion.transform.messaging.handler.MessageHandlerBuilders;
+import com.pushtechnology.diffusion.transform.messaging.handler.MessageHandlerHandle;
+import com.pushtechnology.diffusion.transform.messaging.handler.TransformedMessageHandler;
+import com.pushtechnology.diffusion.transform.messaging.send.tohandler.MessageSender;
+import com.pushtechnology.diffusion.transform.messaging.send.tohandler.MessageToHandlerSenderBuilders;
+import com.pushtechnology.diffusion.transform.transformer.TransformationException;
+import com.pushtechnology.diffusion.transform.transformer.Transformers;
+
+/**
+ * Integration test for sending messages from a session to a handler.
+ * @author Matt Champion 12/04/2017
+ */
+public final class SessionToHandlerIT {
+    @Mock
+    private Session.Listener listener;
+    @Mock
+    private TransformedMessageHandler<Integer> handler;
+    @Mock
+    private SendCallback sendCallback;
+
+    private Session session;
+    private Session controlSession;
+
+    @Before
+    public void setUp() {
+        initMocks(this);
+
+        session = Diffusion
+            .sessions()
+            .listener(listener)
+            .principal("control")
+            .password("password")
+            .open("ws://localhost:8080");
+        verify(listener, timed()).onSessionStateChanged(session, CONNECTING, CONNECTED_ACTIVE);
+
+        controlSession = Diffusion
+            .sessions()
+            .listener(listener)
+            .principal("control")
+            .password("password")
+            .open("ws://localhost:8080");
+        verify(listener, timed()).onSessionStateChanged(controlSession, CONNECTING, CONNECTED_ACTIVE);
+    }
+
+    @After
+    public void postConditions() {
+        session.close();
+        verify(listener, timed()).onSessionStateChanged(session, CONNECTED_ACTIVE, CLOSED_BY_CLIENT);
+        controlSession.close();
+        verify(listener, timed()).onSessionStateChanged(controlSession, CONNECTED_ACTIVE, CLOSED_BY_CLIENT);
+
+        verifyNoMoreInteractions(listener, handler, sendCallback);
+    }
+
+    @Test
+    public void sendToHandler() throws TransformationException {
+        final MessageHandlerHandle handle = MessageHandlerBuilders
+            .newMessageHandlerBuilder()
+            .transform(Transformers.<Content>toByteArray())
+            .transform(Transformers.byteArrayToBinary())
+            .transform(Transformers.binaryToInteger())
+            .bind(controlSession)
+            .register("message/path", handler);
+
+        verify(handler, timed()).onRegistered("message/path");
+
+        final MessageSender<Integer> sender = MessageToHandlerSenderBuilders
+            .newMessageSenderBuilder()
+            .transform(Transformers.<Binary, Bytes>cast(Bytes.class))
+            .transform(Transformers.integerToBinary())
+            .build(session);
+
+        sender.send("message/path", 5, sendCallback);
+
+        verify(sendCallback, timed()).onComplete();
+
+        verify(handler, timed()).onMessageReceived(
+            "message/path",
+            5,
+            session.getSessionId(),
+            Collections.<String, String>emptyMap());
+
+        handle.close();
+        verify(handler, timed()).onClose("message/path");
+    }
+
+    private VerificationWithTimeout timed() {
+        return timeout(5000L);
+    }
+}
