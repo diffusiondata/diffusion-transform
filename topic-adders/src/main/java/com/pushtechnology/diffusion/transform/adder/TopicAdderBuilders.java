7b9fb9f3e4ff2ee500d093e5b5822e4e26a5e246
Matt Champion <mattunderscorechampion@gmail.com>
2017-10-07 19:51:30 +0100

    Add a method that allows topic adders to be add from a provided value type.

--- topic-adders/src/main/java/com/pushtechnology/diffusion/transform/adder/TopicAdderBuilders.java
+++ topic-adders/src/main/java/com/pushtechnology/diffusion/transform/adder/TopicAdderBuilders.java
@@ -18,8 +18,10 @@ package com.pushtechnology.diffusion.transform.adder;
 import static com.pushtechnology.diffusion.transform.transformer.Transformers.identity;
 
 import com.pushtechnology.diffusion.client.topics.details.TopicType;
+import com.pushtechnology.diffusion.datatype.Bytes;
 import com.pushtechnology.diffusion.datatype.binary.Binary;
 import com.pushtechnology.diffusion.datatype.json.JSON;
+import com.pushtechnology.diffusion.datatype.recordv2.RecordV2;
 
 /**
  * Factory for creating instances of {@link TopicAdderBuilder}s.
@@ -28,6 +30,17 @@ import com.pushtechnology.diffusion.datatype.json.JSON;
  */
 public final class TopicAdderBuilders {
     private TopicAdderBuilders() {
+    }
+
+    /**
+     * Create a builder for {@link JSON} topic adders.
+     * @param valueType The value type
+     * @param <V> The value type
+     * @return The builder
+     * @throws IllegalArgumentException If there is no known topic type for the value
+     */
+    public static <V extends Bytes> UnboundSafeTopicAdderBuilder<V, V> topicAdderBuilder(Class<V> valueType) {
+        return new UnboundSafeTopicAdderBuilderImpl<>(topicTypeFromValueType(valueType), identity(valueType));
     }
 
     /**
@@ -44,5 +57,33 @@ public final class TopicAdderBuilders {
      */
     public static UnboundSafeTopicAdderBuilder<Binary, Binary> binaryTopicAdderBuilder() {
         return new UnboundSafeTopicAdderBuilderImpl<>(TopicType.BINARY, identity(Binary.class));
+    }
+
+    /**
+     * Returns the topic type for a given value type.
+     * @throws IllegalArgumentException If there is no known topic type for the value
+     */
+    /*package*/ static TopicType topicTypeFromValueType(Class<?> valueType) {
+        if (JSON.class.isAssignableFrom(valueType)) {
+            return TopicType.JSON;
+        }
+        else if (Binary.class.isAssignableFrom(valueType)) {
+            return TopicType.BINARY;
+        }
+        else if (String.class.isAssignableFrom(valueType)) {
+            return TopicType.STRING;
+        }
+        else if (Double.class.isAssignableFrom(valueType)) {
+            return TopicType.DOUBLE;
+        }
+        else if (Long.class.isAssignableFrom(valueType)) {
+            return TopicType.INT64;
+        }
+        else if (RecordV2.class.isAssignableFrom(valueType)) {
+            return TopicType.RECORD_V2;
+        }
+        else {
+            throw new IllegalArgumentException("The topic type for " + valueType + " is unknown");
+        }
     }
 }
