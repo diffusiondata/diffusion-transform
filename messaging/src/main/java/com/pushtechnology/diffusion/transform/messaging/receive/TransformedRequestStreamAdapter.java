f70272664b63b9ba7168a473d969b45b70a99f09
Matt Champion <mattunderscorechampion@gmail.com>
2017-10-13 22:53:26 +0100

    Add adapter for RequestStream to TransformedRequestStream.

---
+++ messaging/src/main/java/com/pushtechnology/diffusion/transform/messaging/receive/TransformedRequestStreamAdapter.java
@@ -0,0 +1,86 @@
+/*******************************************************************************
+ * Copyright (C) 2017 Push Technology Ltd.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *******************************************************************************/
+
+package com.pushtechnology.diffusion.transform.messaging.receive;
+
+import com.pushtechnology.diffusion.client.callbacks.ErrorReason;
+import com.pushtechnology.diffusion.client.features.Messaging;
+import com.pushtechnology.diffusion.transform.transformer.TransformationException;
+import com.pushtechnology.diffusion.transform.transformer.Transformer;
+
+/**
+ * Adapter from {@link Messaging.RequestStream} to {@link TransformedRequestStream}.
+ *
+ * @param <S> the type of request understood by Diffusion
+ * @param <T> the type of response understood by Diffusion
+ * @param <U> the type of request
+ * @param <V> the type of response
+ * @author Push Technology Limited
+ */
+/*package*/ final class TransformedRequestStreamAdapter<S, T, U, V> implements Messaging.RequestStream<S, T> {
+    private final Transformer<S, U> requestTransformer;
+    private final Transformer<V, T> responseTransformer;
+    private final TransformedRequestStream<S, U, V> delegate;
+
+    /*package*/ TransformedRequestStreamAdapter(
+        Transformer<S, U> requestTransformer,
+        Transformer<V, T> responseTransformer,
+        TransformedRequestStream<S, U, V> delegate) {
+
+        this.requestTransformer = requestTransformer;
+        this.responseTransformer = responseTransformer;
+        this.delegate = delegate;
+    }
+
+    @Override
+    public void onRequest(String path, S request, Responder<T> responder) {
+        final TransformedResponder transformedResponder = new TransformedResponder(responder);
+
+        try {
+            delegate.onRequest(path, requestTransformer.transform(request), transformedResponder);
+        }
+        catch (TransformationException e) {
+            delegate.onTransformationException(path, request, transformedResponder, e);
+        }
+    }
+
+    @Override
+    public void onClose() {
+        delegate.onClose();
+    }
+
+    @Override
+    public void onError(ErrorReason errorReason) {
+        delegate.onError(errorReason);
+    }
+
+    private final class TransformedResponder implements TransformedRequestStream.Responder<V> {
+        private final Responder<T> responder;
+
+        TransformedResponder(Responder<T> responder) {
+            this.responder = responder;
+        }
+
+        @Override
+        public void respond(V response) throws TransformationException {
+            responder.respond(responseTransformer.transform(response));
+        }
+
+        @Override
+        public void reject(String errorMessage) {
+            responder.reject(errorMessage);
+        }
+    }
+}
---
+++ messaging/src/test/java/com/pushtechnology/diffusion/transform/messaging/receive/TransformedRequestStreamAdapterTest.java
@@ -0,0 +1,146 @@
+/*******************************************************************************
+ * Copyright (C) 2017 Push Technology Ltd.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *******************************************************************************/
+
+package com.pushtechnology.diffusion.transform.messaging.receive;
+
+import static org.mockito.Matchers.eq;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+import com.pushtechnology.diffusion.client.callbacks.ErrorReason;
+import com.pushtechnology.diffusion.client.features.Messaging;
+import com.pushtechnology.diffusion.datatype.json.JSON;
+import com.pushtechnology.diffusion.transform.transformer.TransformationException;
+import com.pushtechnology.diffusion.transform.transformer.Transformer;
+
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.ExpectedException;
+import org.mockito.ArgumentCaptor;
+import org.mockito.Captor;
+import org.mockito.Mock;
+import org.mockito.junit.MockitoJUnit;
+import org.mockito.junit.MockitoRule;
+
+/**
+ * Unit tests for {@link TransformedRequestStreamAdapter}.
+ *
+ * @author Matt Champion 13/10/2017
+ */
+public final class TransformedRequestStreamAdapterTest {
+    @Rule
+    public MockitoRule mockitoRule = MockitoJUnit.rule();
+
+    @Rule
+    public ExpectedException expectedException = ExpectedException.none();
+
+    @Mock
+    private Transformer<JSON, JSON> requestTransformer;
+    @Mock
+    private Transformer<JSON, JSON> responseTransformer;
+    @Mock
+    private TransformedRequestStream<JSON, JSON, JSON> delegate;
+    @Mock
+    private Messaging.RequestStream.Responder<JSON> responder;
+    @Mock
+    private JSON request;
+    @Mock
+    private JSON response;
+    @Mock
+    private JSON transformedRequest;
+    @Mock
+    private JSON transformedResponse;
+
+    @Captor
+    private ArgumentCaptor<TransformedRequestStream.Responder<JSON>> responderCaptor;
+
+    @Before
+    public void setUp() throws TransformationException {
+        when(requestTransformer.transform(request)).thenReturn(transformedRequest);
+        when(responseTransformer.transform(response)).thenReturn(transformedResponse);
+    }
+
+    @Test
+    public void receiveRequest() throws TransformationException {
+        final TransformedRequestStreamAdapter<JSON, JSON, JSON, JSON> adapter = new TransformedRequestStreamAdapter<>(
+            requestTransformer,
+            responseTransformer,
+            delegate);
+
+        adapter.onRequest("path", request, responder);
+
+        verify(requestTransformer).transform(request);
+
+        verify(delegate).onRequest(eq("path"), eq(transformedRequest), responderCaptor.capture());
+
+        final TransformedRequestStream.Responder<JSON> transformedResponder = responderCaptor.getValue();
+
+        transformedResponder.respond(response);
+        verify(responseTransformer).transform(response);
+        verify(responder).respond(transformedResponse);
+
+        transformedResponder.reject("error");
+        verify(responder).reject("error");
+    }
+
+    @Test
+    public void receiveBadRequest() throws TransformationException {
+        final TransformationException e = new TransformationException("for test");
+        when(requestTransformer.transform(request)).thenThrow(e);
+
+        final TransformedRequestStreamAdapter<JSON, JSON, JSON, JSON> adapter = new TransformedRequestStreamAdapter<>(
+            requestTransformer,
+            responseTransformer,
+            delegate);
+
+        adapter.onRequest("path", request, responder);
+
+        verify(requestTransformer).transform(request);
+
+        verify(delegate).onTransformationException(eq("path"), eq(request), responderCaptor.capture(), eq(e));
+
+        final TransformedRequestStream.Responder<JSON> transformedResponder = responderCaptor.getValue();
+
+        transformedResponder.respond(response);
+        verify(responseTransformer).transform(response);
+        verify(responder).respond(transformedResponse);
+
+        transformedResponder.reject("error");
+        verify(responder).reject("error");
+    }
+
+    @Test
+    public void onError() {
+        final TransformedRequestStreamAdapter<JSON, JSON, JSON, JSON> adapter = new TransformedRequestStreamAdapter<>(
+            requestTransformer,
+            responseTransformer,
+            delegate);
+
+        adapter.onError(ErrorReason.NO_SUCH_SESSION);
+        verify(delegate).onError(ErrorReason.NO_SUCH_SESSION);
+    }
+
+    @Test
+    public void onClose() {
+        final TransformedRequestStreamAdapter<JSON, JSON, JSON, JSON> adapter = new TransformedRequestStreamAdapter<>(
+            requestTransformer,
+            responseTransformer,
+            delegate);
+
+        adapter.onClose();
+        verify(delegate).onClose();
+    }
+}
