a1aba397b4c6aff39bf2f7cd77d2ea720b20f48e
Matt Champion <mattunderscorechampion@gmail.com>
2017-10-21 12:10:54 +0100

    Deprecate method using Transformer in transformer builder.

--- transformers/src/main/java/com/pushtechnology/diffusion/transform/transformer/TransformerBuilder.java
+++ transformers/src/main/java/com/pushtechnology/diffusion/transform/transformer/TransformerBuilder.java
@@ -32,7 +32,9 @@ public interface TransformerBuilder<S, T> {
      * @param newTransformer the new transformer
      * @param <R> the new type of the transformed values
      * @return a new transformer builder
+     * @deprecated since 2.0.0 in favour of methods using unsafe transformer
      */
+    @Deprecated
     <R> TransformerBuilder<S, R> transform(Transformer<T, R> newTransformer);
 
     /**
